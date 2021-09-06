d778226ae914cde56c8a2a1acb41e2a5a3ab353a
Victor Popkov <victor@popkov.me>
2021-09-06 04:22:45 +0300

    Improve code consistency in unix exported

--- pkg/unix/mod_tools/exported/ResizeInfo.py
+++ pkg/unix/mod_tools/exported/ResizeInfo.py
@@ -3,27 +3,28 @@
 # everything in the hud to keep 1:1 sizing
 #
 # Filenames override prefab settings
-
 import collections
 
+
 def DefaultSize():
-	return 1.0
+    return 1.0
 
-SizeMap = collections.defaultdict( DefaultSize )
-SizeMap[ "hud" ] = 1.0
-SizeMap[ "frontend" ] = 1.0
-SizeMap[ "carrot" ] = .5
-SizeMap[ "fireflies" ] = .5
-SizeMap[ "flies" ] = .5
 
-SizeMap[ "ice_over.zip" ] = 1.0
-SizeMap[ "vig.zip" ] = 1.0
-SizeMap[ "clouds_ol.zip" ] = 0.25
-SizeMap[ "fire_over.zip" ] = 0.5
-SizeMap[ "button.zip" ] = 1.0
-SizeMap[ "button_small.zip" ] = 1.0
-SizeMap[ "button_long.zip" ] = 1.0
+SizeMap = collections.defaultdict(DefaultSize)
+SizeMap["hud"] = 1.0
+SizeMap["frontend"] = 1.0
+SizeMap["carrot"] = .5
+SizeMap["fireflies"] = .5
+SizeMap["flies"] = .5
 
-SizeMap[ "creepy_hands.zip" ] = 1.0
-SizeMap[ "generating_cave.zip" ] = 1.0
-SizeMap[ "generating_world.zip" ] = 1.0
+SizeMap["ice_over.zip"] = 1.0
+SizeMap["vig.zip"] = 1.0
+SizeMap["clouds_ol.zip"] = 0.25
+SizeMap["fire_over.zip"] = 0.5
+SizeMap["button.zip"] = 1.0
+SizeMap["button_small.zip"] = 1.0
+SizeMap["button_long.zip"] = 1.0
+
+SizeMap["creepy_hands.zip"] = 1.0
+SizeMap["generating_cave.zip"] = 1.0
+SizeMap["generating_world.zip"] = 1.0
